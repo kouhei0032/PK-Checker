7dea1049c51067a5fec22d064574e2902307881b
kouhei <kouhei0032@gmail.com>
2026-04-27 21:54:17 +0900

    Update main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="PK-Checker", page_icon="📚")
+
+st.title("リンカチェッカー")
+st.write("日々の献立管理における、リン(P)とカリウム(K)の計算をサポートします。")
+
+st.info("""
+### 💡 このアプリでできること
+- **食材別の成分確認**: 文部科学省のデータベースに基づいた正確な計算。
+- **摂取量の見える化**: 制限値を超えないためのスマートなチェック。
+""")
+
+st.write("Developed by Kohei Takahashi")
