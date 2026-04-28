598903899f7f7417336d8670c16e8e035b02a3fe
kouhei <kouhei0032@gmail.com>
2026-04-28 11:09:01 +0900

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -1,17 +1,6 @@
 import streamlit as st
 
-st.markdown("""
-    <style>
-    .no-wrap {
-        white-space: nowrap;
-        font-size: 2.25rem; /* st.titleに近いサイズ */
-        font-weight: 700;
-        margin-bottom: 1.5rem;
-    }
-    </style>
-    <div class="no-wrap">🥗 リンカチェッカー</div>
-    """, unsafe_content_safe=True)
-
+st.title("🥗 リンカチェッカー")
 st.write("日々の献立管理における、リン(P)とカリウム(K)の計算をサポートします。")
 
 st.info("""
